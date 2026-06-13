70e1ba096444be81b6ae3699225d0793124002c6
MarianoEs <marianoesser@gmail.com>
2026-06-13 18:47:32 -0300

    Entidad Cotización e Item:Cotización

---
+++ IBUY.BD/Datos/Entity/Cotización.cs
@@ -0,0 +1,29 @@
+﻿using Proyecto2026.BD.Datos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBUY.BD.Datos.Entity
+{
+    public class Cotización
+    {
+        public class Cotizacion : EntityBase
+        {
+
+            public string Estado { get; set; }
+
+            public string PlazoEntrega { get; set; }
+
+            public DateTime FechaEnvio { get; set; }
+
+            public int NotaPedidoId { get; set; }
+
+            public NotaPedido NotaPedido { get; set; }
+
+            public int EmpresaProveedoraId { get; set; }
+
+            public Empresa EmpresaProveedora { get; set; }
+
+        }
+    }
+}
